19cd99c6832fc3a1a2d86726f35ad2186023e062
Prince <pp275688@gmail.com>
2025-01-23 17:21:14 +0530

    deleting user, will start work on weather app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-// import { UsersModule } from './users/users.module';
-import { UsersController } from './users/users.controller';
-import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule],
-  controllers: [AppController, UsersController],
+  imports: [],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
